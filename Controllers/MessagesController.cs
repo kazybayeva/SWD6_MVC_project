066a81fcb8fe146f4c62d254ac685d03db8bc8e1
Madina <madinaxk@gmail.com>
2019-10-12 15:36:14 +0600

    'tutorial#2'

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorial1.Controllers
+{
+    [Route("Say")]
+    public class MessagesController : Controller
+    {
+        [Route("{*message}")]
+        public IActionResult ShowMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                ViewData["Message"] = "Message Is Empty!";
+            } else {
+                ViewData["Message"] = message;
+            }
+
+            return View();
+        }
+    }
+}
